0647ff3949c06215ab958304df9b13a67cef93e2
stellaratlas8 <ajmalinao1@gmail.com>
2023-04-16 23:08:22 +0800

    basics to mongodb and some error handling

--- index.js
+++ index.js
@@ -1,86 +1,96 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const Entry = require("./models/entry");
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use(cors());
 morgan.token("content", function (req, res) {
   if (req.method == "POST") return JSON.stringify(req.body);
   return null;
 });
-
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :content"
-  )
+const requestLogger = morgan(
+  ":method :url :status :res[content-length] - :response-time ms :content"
 );
 
+// I like this middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+// app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+app.use(requestLogger);
 
-let entries = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-const generateId = () => {
-  let num;
-  num = Math.floor(Math.random() * 9999);
-  while (entries.find((entry) => entry.id === num)) {
-    num = Math.floor(Math.random() * 9999);
-  }
-  return num;
-};
+// const generateId = () => {
+//   let num;
+//   num = Math.floor(Math.random() * 9999);
+//   while (entries.find((entry) => entry.id === num)) {
+//     num = Math.floor(Math.random() * 9999);
+//   }
+//   return num;
+// };
 
 app.get("/api/persons", (request, response) => {
-  response.status(200).json(entries).end();
+  Entry.find({})
+    .then((entries) => {
+      response.status(200).json(entries).end();
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(400).end();
+    });
 });
 
 app.get("/info", (request, response) => {
-  response
-    .status(200)
-    .send(
-      `<p>Phonebook has info for ${entries.length} people</p><p>${Date()}</p>`
-    )
-    .end();
+  Entry.countDocuments({}, err, (count) => {
+    response
+      .status(200)
+      .send(`<p>Phonebook has info for ${count} people</p><p>${Date()}</p>`)
+      .end();
+  }).catch((err) => {
+    console.log(err);
+    response.status(400).end();
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const found = entries.find((entry) => entry.id === Number(request.params.id));
-
-  if (found) response.status(200).json(found).end();
-  else response.status(404).end();
+  Entry.findByIdAndDelete(request.params.id)
+    .then((entry) => {
+      if (entry) response.status(200).json(entry).end();
+      else response.status(404).end();
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(400).end();
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const found = entries.find((entry) => entry.id === id);
-  entries = entries.filter((entry) => entry.id !== id);
-
-  if (found) response.status(204).end();
-  else response.status(404).end();
+  Entry.findById(request.params.id)
+    .then((entry) => {
+      if (entry) response.status(204).end();
+      else response.status(404).end();
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(400).end();
+    });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -90,20 +100,33 @@ app.post("/api/persons", (request, response) => {
     return;
   }
 
-  const nameExists = entries.find((entry) => entry.name === data.name);
-  if (nameExists) {
-    response.status(400).json({ error: "Name must be unique" }).end();
-    return;
-  }
+  Entry.find({ name: data.name })
+    .then((found) => {
+      if (found.length > 0) {
+        response.status(400).json({ error: "Name must be unique" }).end();
+        return;
+      }
 
-  let entry = {
-    ...data,
-    id: generateId(),
-  };
-  entries = entries.concat(entry);
+      let entry = new Entry(data);
 
-  response.json(entry).end();
+      entry
+        .save()
+        .then((savedNote) => {
+          response.json(savedNote).end();
+        })
+        .catch((err) => {
+          console.log(err);
+          response.status(400).end();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(400).end();
+    });
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
